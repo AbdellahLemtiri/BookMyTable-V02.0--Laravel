ac3f79833ea0c96d0463a7b800b84b6868d3d87d
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2026-02-13 10:35:18 +0100

    feat: add payment model relationships

--- BookMyTableV1/app/Models/Paiement.php
+++ BookMyTableV1/app/Models/Paiement.php
@@ -2,9 +2,31 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Paiement extends Model
 {
     //
+ use HasFactory;
+    protected $fillable = [
+        'reservation_id',
+        'montant',
+        'methode_paiement',
+        'statut',
+        'date_paiement',
+        'transaction_id'
+    ];
+
+
+    protected $casts = ['date_paiement'=>'date'];
+    public function Reservation()
+    {
+        return $this->belongsTo(Reservation::class);
+    }
+      public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
