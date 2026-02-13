a7f08d383779a17e1849d648a909292470506251
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2026-02-13 10:35:18 +0100

    feat: update reservation model fillable fields

--- BookMyTableV1/app/Models/Reservation.php
+++ BookMyTableV1/app/Models/Reservation.php
@@ -11,7 +11,34 @@ class Reservation extends Model
     use HasFactory;
 
     protected $fillable = [
-        ''
-    ]
-    
+        'user_id',          
+        'restaurant_id',    
+        'date_reservation',
+        'heure_reservation',
+        'nb_personnes',
+        'statut',
+        'total_price',
+        'qr_code'
+    ];
+ 
+    protected $casts = [
+        'date_reservation' => 'date',
+        'total_price' => 'float',
+        'nb_personnes' => 'integer',
+    ];
+ 
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+ 
+    public function restaurant()
+    {
+        return $this->belongsTo(Restaurant::class);
+    }
+ 
+    public function paiement()
+    {
+        return $this->hasOne(Paiement::class);
+    }
 }
